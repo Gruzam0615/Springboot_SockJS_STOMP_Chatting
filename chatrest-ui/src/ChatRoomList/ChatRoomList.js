2801d44d293ec0893fcba4ce04e2c3fa4902a3d9
idenit2206 <idenit2206@iden-it.com>
2022-07-22 18:26:09 +0900

    STOMP 채팅 REACT 테스트중

--- chatrest-ui/src/ChatRoomList/ChatRoomList.js
+++ chatrest-ui/src/ChatRoomList/ChatRoomList.js
@@ -1,13 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { over } from "stompjs";
 import SockJS from "sockjs-client";
 
 const ChatRoomList = () => {
 
     const [ chatRoomsList, setChatRoomsList ] = useState([]);
+    const [ roomNameValue, setRoomNameValue ] = useState("");
+    const roomName = useRef();
 
     useEffect(() => {
     }, []);
+
+    const createChatRoomList = () => {
+        const URL = `/chat/room?roomName=${roomName}&usersIdx=1`;
+        const OPTIONS = {
+            method: "POST",
+            redirect: "follow"
+        }
+
+        fetch(URL, OPTIONS)
+    }
 
     const getChatRoomsList = () => {
         const URL = "/chat/room";
@@ -15,9 +27,12 @@ const ChatRoomList = () => {
             method: "GET",
             redirect: "follow"
         }
-        return fetch(URL, OPTIONS)
+
+        fetch(URL, OPTIONS)
             .then(response => response.json())
-            .then(result => console.log(result))
+            .then(result => { 
+                console.log(result)
+            })
             .catch(error => console.log('error', error));
     }
 
@@ -25,6 +40,12 @@ const ChatRoomList = () => {
         <div className="ChatRoomList">
             <h3>채팅방 목록</h3>
             <button onClick={getChatRoomsList}>목록 불러오기</button>
+            <br/>
+            <div>
+                <label htmlFor="roomName">채팅방 제목</label>
+                <input type="text" className="" name="roomName" onChange={CustomInputChange} value={roomNameValue}></input>
+                <input type="button" value="만들기" onClick={createChatRoomList}/>
+            </div>
         </div>
     );
 }
